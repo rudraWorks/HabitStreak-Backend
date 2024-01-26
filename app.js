16826db439706b09b94db085d8f4991cf0871ad3
Rudra Pratap Singh <merudra.official@gmail.com>
2024-01-26 19:29:33 +0530

    change port number

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import notesRoutes from './routes/notes.js'
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
