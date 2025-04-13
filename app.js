564420b52a11879946a97d85cd844cf3cd00f59c
Rudra Pratap Singh <srudra754@gmail.com>
2025-04-13 23:37:47 +0530

    fix CORS

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: '*', // or '*' for all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['*'], // will work in most browsers
+  optionsSuccessStatus: 204 // to handle legacy browsers
+}));
+
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
